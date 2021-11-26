9934e359883560ceb299aa4ed444ab2aa0cc3fbc
pgmedeiros <emaildopedromedeiros@gmail.com>
2021-11-26 13:28:37 -0300

    tudo ok

--- src/main/java/com/example/crud/controller/Config.java
+++ src/main/java/com/example/crud/controller/Config.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
+
     @Bean
     public Usuario usuario(){
         return new Usuario();
--- src/main/java/com/example/crud/controller/Controller.java
+++ src/main/java/com/example/crud/controller/Controller.java
@@ -3,6 +3,8 @@ package com.example.crud.controller;
 import com.example.poc_swagger_gen_with_springboot.rest.api.UsuarioApi;
 import com.example.poc_swagger_gen_with_springboot.rest.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,12 @@ public class Controller{
     @Autowired
     Usuario usuario;
 
-    @GetMapping("/{name}")
-    public String get(@PathVariable("name") String name){
-        usuario.setNome(name);
+
+    @GetMapping("/")
+    public String usuarioGet() {
+        usuario.setNome("José Almeida");
         return usuario.getNome();
     }
+
 
 }
